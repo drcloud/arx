276395d9373e65700e2fdc219e944b37c5815887
Jason Dusek <jason.dusek@gmail.com>
2016-04-09 02:49:32 -0700

    Provide interpreter for sources

--- arx/sources/test/inline.py
+++ arx/sources/test/inline.py
@@ -14,12 +14,14 @@ def test_b64():
 
 
 def test_b64_targz():
-    # Contains one file `x/x` with contents `x`.
-    small_tar_ball = """
-        H4sIAFbACFcAA+3RSwrCMBSF4Tt2FdlBb9I81lOUgigKRiHLN9jSQSkdCEGE/5uc
-        QUJykls6aU5VUwjmk3FKdX7KmbEuqbO+LvRGrYvWigntq4m88nN41Cr5fj2f8m04
-        Xrb31W3juHPO/I4l/0TpSvM76n9E7/fm79bzT6kXo82bCfM//LoBAAAAAAAAAAAA
-        AAAAgG+9AdEJedgAKAAA
-    """
-    source = InlineTarGZ.base64(small_tar_ball)
+    source = InlineTarGZ.base64(small_tgz)
     assert isinstance(source, InlineTarGZ)
+
+
+# Contains one file `x/x` with contents `x`.
+small_tgz = """
+    H4sIAFbACFcAA+3RSwrCMBSF4Tt2FdlBb9I81lOUgigKRiHLN9jSQSkdCEGE/5uc
+    QUJykls6aU5VUwjmk3FKdX7KmbEuqbO+LvRGrYvWigntq4m88nN41Cr5fj2f8m04
+    Xrb31W3juHPO/I4l/0TpSvM76n9E7/fm79bzT6kXo82bCfM//LoBAAAAAAAAAAAA
+    AAAAgG+9AdEJedgAKAAA
+"""
